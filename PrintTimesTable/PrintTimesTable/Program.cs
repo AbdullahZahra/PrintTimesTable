1f45998f68a174d3b0d06fe2de6f8dee4d1f0969
Abdullah Zahra <60556442+AbdullahZahra@users.noreply.github.com>
2021-05-03 16:40:47 +0200

    Add files via upload

---
+++ PrintTimesTable/PrintTimesTable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintTimesTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimesTable.RunTimesTable();
+        }
+    }
+}
---
+++ PrintTimesTable/PrintTimesTable/TimesTable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintTimesTable
+{
+    class TimesTable
+    {
+        public static void timesTable(byte Number) // The function to show the times table
+        {
+            Console.WriteLine($"This is {Number} times table:");
+
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{Number} x {i} = {Number * i}");
+            }
+        }
+
+        public static void RunTimesTable()
+        {
+            while (true)
+            {
+                Console.Write("Please, enter a number: ");
+                try
+                {
+                    byte Number = byte.Parse(Console.ReadLine());
+                    timesTable(Number);
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input.");
+                    continue;
+                }
+
+
+                Console.WriteLine("Do you want to terminate the program?"); // A condition to terminate the program 
+
+                Console.WriteLine("(1) Yes.");
+                Console.WriteLine("(2) No.");
+
+                byte condition = byte.Parse(Console.ReadLine());
+
+                switch (condition)
+                {
+                    case 1:
+                        continue;
+                    case 2:
+                        return;
+                    default:
+                        continue;
+                }
+            }
+        }
+    }
+}
